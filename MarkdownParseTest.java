b08a4d5046736c7cc4f56904288fa59cb6956e5a
r-chau <97642931+r-chau@users.noreply.github.com>
2022-02-02 18:19:26 -0800

    Update MarkdownParseTest.java
    fixing break

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -4,15 +4,24 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.ArrayList;
 
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
-        assertEquals(99999999, 1 + 1);
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testFile1() throws IOException {
+        String contents= Files.readString(Path.of("./test-file.md"));
+        List<String> expect = List.of("https://something.com", "some-page.html");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
     }
 }
+
 
 //javac -cp ".;lib/junit-4.13.2.jar;lib\hamcrest-core-1.3.jar"  MarkdownParseTest.java
 //java -cp ".;lib/junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest 
