cf35c02f315688855d8d769357bf629976a23816
r-chau <97642931+r-chau@users.noreply.github.com>
2022-02-25 12:36:44 -0800

    adding snippet tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,6 +13,33 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
    
+    @Test
+    public void testFile1() throws IOException {
+        String contents = Files.readString(Path.of("./test-file.md"));
+        List<String> expect = List.of("https://something.com", "some-page.html");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet1() throws IOException {
+        String contents = Files.readString(Path.of("./snippet1.md"));
+        List<String> expect = List.of("`google.com", "google.com", "ucsd.edu");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        String contents = Files.readString(Path.of("./snippet2.md"));
+        List<String> expect = List.of("a.com", "a.com(())", "example.com");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        String contents = Files.readString(Path.of("./snippet3.md"));
+        List<String> expect = List.of("https://www.twitter.com", "https://ucsd-15l-w22.github.io/", "https://cse.ucsds.edu");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
 }
 
 
